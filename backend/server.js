491d18c727881ced32d2dad8ec8852af7aa1403f
Gustav Svensson <gustav.svensson99@hotmail.com>
2022-05-26 18:46:03 +0200

    add pocess.env.PORT in server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const app = express()
-const PORT = 1178
+const PORT = process.env.PORT || 1178
 import hamsters from './routes/hamster.js'
 
 // console.log('Log ' + __dirname);
